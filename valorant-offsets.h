e426c953f6dc1acd1440da41ec29e53d12712036
Jackson <72171280+Pinguu-dev@users.noreply.github.com>
2023-02-15 13:11:39 +0100

    06.03.00.828381

--- valorant-offsets.h
+++ valorant-offsets.h
@@ -1,26 +1,26 @@
 namespace offsets {
   // General Offsets
-  uintptr_t uworld_state          =     0x912B300;             // @image_base
+  uintptr_t uworld_state          =     0x9182500;             // @image_base
   uintptr_t uworld_key            =     uworld_state + 0x38;   // @image_base + (uworld_state + 0x38)
   
-  uintptr_t gobj_state            =     0x8F98BC0;             // @image_base
+  uintptr_t gobj_state            =     0x8FEFDC0;             // @image_base
   uintptr_t gobj_key              =     gobj_state + 0x38;     // @image_base + (gobj_state + 0x38)
   
-  uintptr_t find_object           =     0x340F450;             // @image_base
-  uintptr_t process_event         =     0x33D1240;             // @image_base
+  uintptr_t find_object           =     0x34435A0;             // @image_base
+  uintptr_t process_event         =     0x3405390;             // @image_base
   
-  uintptr_t bone_matrix           =     0x4A54E60;             // @image_base
+  uintptr_t bone_matrix           =     0x4A893B0;             // @image_base
   
-  uintptr_t line_of_sight         =     0x4A88540;             // @image_base
-  uintptr_t skin_changer_decrypt  =     0x2466060;             // @image_base
-  uintptr_t veh_deref             =     0x1BF5BC0;             // deref_pointer_in_game_space_fn - https://www.unknowncheats.me/forum/valorant/503616-dumping-valorant-perfect-results-easy.html
+  uintptr_t line_of_sight         =     0x4ABCA90;             // @image_base
+  uintptr_t skin_changer_decrypt  =     0x2487C30;             // @image_base
+  uintptr_t veh_deref             =     0x1C15C00;             // deref_pointer_in_game_space_fn - https://www.unknowncheats.me/forum/valorant/503616-dumping-valorant-perfect-results-easy.html
 
-  uintptr_t enemy_outline_color   =     0x9045C20;             // @image_base (FLinearColor)
-  uintptr_t ally_outline_color    =     0x9044340;             // @image_base (FLinearColor)
-  uintptr_t ares_outline_mode     =     0x4A06660;             // @image_base
+  uintptr_t enemy_outline_color   =     0x909CD20;             // @image_base (FLinearColor)
+  uintptr_t ally_outline_color    =     0x909C790;             // @image_base (FLinearColor)
+  uintptr_t ares_outline_mode     =     0x4A3ABB0;             // @image_base
  
-  uintptr_t proj_world_to_screen  =     0x4BA0060;             // @image_base (ProjectWorldToScreen)
-  uintptr_t get_actor_bounds      =     0x4838730;             // @image_base
+  uintptr_t proj_world_to_screen  =     0x4BD4300;             // @image_base (ProjectWorldToScreen)
+  uintptr_t get_actor_bounds      =     0x486CC80;             // @image_base
   
   // VTables
   
